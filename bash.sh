886c77452f31c3b9d382f2fa1b419ea5f2bf1043
montusec <87820773+montusec@users.noreply.github.com>
2021-07-27 08:38:29 +0000

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,4 +2,4 @@
 
 
 
-wget https://az764295.vo.msecnd.net/stable/c3f126316369cd610563c75b1b1725e0679adfb3/code_1.58.2-1626302803_amd64.deb| sudo dpkg-i
+wget https://az764295.vo.msecnd.net/stable/c3f126316369cd610563c75b1b1725e0679adfb3/code_1.58.2-1626302803_amd64.deb| sudo dpkg -i
